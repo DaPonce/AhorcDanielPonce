b620d74249ac79ffbbe19f9e1b2f722755550528
dPons <danielponcegranado@gmail.com>
2017-11-05 19:07:47 +0100

    Implemented styles Cambiado el Theme de la app a uno con tonos verdes. Cambiado el aspecto del botón nueva palabra con un Selector y dos Shape. Cambiada la imagen de vacío.

--- app/src/main/java/com/daniel/ponce/MainActivity.java
+++ app/src/main/java/com/daniel/ponce/MainActivity.java
@@ -1,13 +1,18 @@
 package com.daniel.ponce;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
